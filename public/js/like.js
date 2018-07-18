e2bfd22b78c90304e9697e40d0b28af8ed5244e4
unknown <ms0683436@gmail.com>
2018-07-18 20:36:57 +0800

    fix like ajax url

--- public/js/like.js
+++ public/js/like.js
@@ -1,7 +1,7 @@
 function likeFunction(object_id, object) {
     var isActive = document.getElementById('like'+object_id).getAttribute("aria-pressed");
     var type = (isActive == 'true') ? 'DELETE' : 'POST';
-    var route = (isActive == 'true') ? "/blog_laravel/public/unlike" : "/blog_laravel/public/like";
+    var route = (isActive == 'true') ? "unlike" : "like";
     // console.log(isActive)
     $.ajax({         
         type: type,
